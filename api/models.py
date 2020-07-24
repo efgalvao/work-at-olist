a08274fe036cbf30f9da7f666325649915eac5b7
efgalvao <solfieri@hotmail.com>
2020-07-23 23:55:53 -0300

    Manytomany corrected.

--- api/models.py
+++ api/models.py
@@ -2,17 +2,23 @@ from django.db import models
 
 
 class Author(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, null=False, blank=False)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ['name']
 
 
 class Book(models.Model):
-    name = models.CharField(max_length=50)
-    edition = models.CharField(max_length=10)
-    publication_year = models.IntegerField()
-    authors = models.ManyToManyField(Author, related_name='books')
+    name = models.CharField(max_length=50, null=False, blank=False)
+    edition = models.CharField(max_length=10, null=False, blank=False)
+    publication_year = models.IntegerField(null=False, blank=False)
+    authors = models.ManyToManyField(Author, related_name='books', blank=False)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ['name']
--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,25 @@
 from rest_framework import serializers
 from .models import Author, Book
 
-class BookSerializer(serializers.ModelSerializer):
+class BookCreateSerializer(serializers.ModelSerializer):
+    
+    class Meta(object):
+        model = Book
+        fields = ['id', 'name', 'edition', 'publication_year', 'authors']
+
+class BookListSerializer(serializers.ModelSerializer):
+    #authors =  serializers.StringRelatedField(source='authors', many=True)
 
     class Meta(object):
         model = Book
-        fields = '__all__'
+        fields = ['id', 'name', 'edition', 'publication_year', 'authors']
+
+class BookSearchSerializer(serializers.ModelSerializer):
+    authors = serializers.StringRelatedField(many=True)
+
+    class Meta(object):
+        model = Book
+        fields = ['id', 'name', 'edition', 'publication_year', 'authors']
 
 class AuthorSerializer(serializers.ModelSerializer):
     books = serializers.StringRelatedField(many=True)
@@ -13,4 +27,7 @@ class AuthorSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = Author
         fields = ('name', 'books')
+
+
+
 
